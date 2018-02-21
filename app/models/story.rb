a110414384f6c4a582c05dab6433e5a4110c7008
Madelene Campos <madelenecampos@gmail.com>
2018-02-21 16:49:26 -0500

    Creates story method to get a single story object

--- app/models/story.rb
+++ app/models/story.rb
@@ -3,7 +3,12 @@ class Story < ApplicationRecord
   def self.top_stories
     client = HackernewsRuby::Client.new
     stories = client.top_stories.take(10)
-    stories.map { |s| client.get_item(s) }
+    top_stories = stories.map { |s| client.get_item(s) }
+  end
+
+  def self.story id
+    client = HackernewsRuby::Client.new
+    story = client.get_item(id)
   end
 
 end
