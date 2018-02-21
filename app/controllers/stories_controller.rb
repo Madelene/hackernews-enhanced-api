dd5050ba30a2527252dfe5f54f8dd5553b42de9e
Madelene Campos <madelenecampos@gmail.com>
2018-02-21 16:50:09 -0500

    Exposes the id as a param coming from the outside and uses that variable. Tests in postman

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,10 +1,13 @@
 class StoriesController < ApplicationController
 
+	 expose(:id) { params[:id] }
+
   def index
   	render json: { top_stories: Story.top_stories }
   end
 
   def show
+  	render json: { story: Story.story(id) }
   end
 
 end
