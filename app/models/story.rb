a40e16411a5bae4599f9237fdcffbf950e953e02
Madelene Campos <madelenecampos@gmail.com>
2018-02-21 20:18:17 -0500

    Returns 10 comments for single story

--- app/models/story.rb
+++ app/models/story.rb
@@ -8,7 +8,19 @@ class Story < ApplicationRecord
 
   def self.story id
     client = HackernewsRuby::Client.new
-    story = client.get_item(id)
+    story = client.get_item(id) 
+    payload = 
+      {
+        by: story.by,
+        descendents: story.descendents,
+        id: story.id,
+        comments: story.kids.take(10),
+        score: story.score,
+        time: story.time,
+        title: story.title,
+        type: story.type,
+        url: story.url
+      }
   end
 
 end
