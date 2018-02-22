9ddec8663753c587542d8cb2d023a96a29a21c85
Madelene Campos <madelenecampos@gmail.com>
2018-02-22 16:59:54 -0500

    Exposes params and calls sort method

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,9 +1,13 @@
 class StoriesController < ApplicationController
 
-	expose(:id) { params[:id] }
+	expose(:id)       { params[:id] }
+	expose(:title)    { params[:title] }
+	expose(:by)       { params[:by] }
+	expose(:score)    { params[:score] }
+	expose(:count)    { params[:count] }
 
   def index
-  	render json: { top_stories: Story.top_stories }
+  	render json: { top_stories: Story.sort(title, by, score, count) }
   end
 
   def show
